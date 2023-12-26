7b3af430fc60fd049fcedf89345a67484de237b4
RobertVacaru <vacaru2019@gmail.com>
2023-12-26 15:22:47 +0200

    daily statistics calculated

--- app/Services/StatisticsService.php
+++ app/Services/StatisticsService.php
@@ -6,6 +6,8 @@ use App\Models\DailyStatistics;
 use App\Models\Statistics;
 use App\Models\User;
 use App\Models\Workout;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Collection;
 
 class StatisticsService
 {
@@ -23,15 +25,49 @@ class StatisticsService
         $startingHours = $finishingHours = $preferredWorkouts = [];
         foreach ($workouts as $workout){
             isset($startingHours[$workout->startingHour]) ? $startingHours[$workout->startingHour]++ : $startingHours[$workout->startingHour] = 0;
-            isset( $finishingHours[$workout->finishHour]) ? $finishingHours[$workout->finishHour]++ : $finishingHours[$workout->finishHour] = 0;
-            isset( $preferredWorkouts[$workout->type]) ? $preferredWorkouts[$workout->type]++ : $preferredWorkouts[$workout->type] = 0;
+            isset($finishingHours[$workout->finishHour]) ? $finishingHours[$workout->finishHour]++ : $finishingHours[$workout->finishHour] = 0;
+            isset($preferredWorkouts[$workout->type]) ? $preferredWorkouts[$workout->type]++ : $preferredWorkouts[$workout->type] = 0;
         }
 
         $statistics->preferredStartingHour = array_search(max($startingHours), $startingHours);
         $statistics->preferredFinishingHour = array_search(max($finishingHours), $finishingHours);
         $statistics->preferredWorkout = array_search(max($preferredWorkouts), $preferredWorkouts);
 
-//        $statistics->dailyStatistics()->save(new DailyStatistics());
+        $this->calculateDailyStatistics($statistics, $workouts);
         $statistics->save();
+    }
+
+    public function calculateDailyStatistics(Statistics $statistics, Collection $workouts): void
+    {
+        $dailyArray = [];
+        /**@var Workout $workout*/
+        foreach ($workouts as $workout){
+            $day = $workout->date;
+            $day = Carbon::parse($day)->dayName;
+            $dailyArray[$day][] = $workout;
+        }
+
+        foreach ($dailyArray as $day => $workoutsBasedOnDay){
+            /**@var DailyStatistics $daily*/
+            $daily = DailyStatistics::all()->where(['day' => $day, 'parentId' => $statistics->id])->first();
+            if(!$daily){
+                $daily = new DailyStatistics();
+                $daily->day = $day;
+                $daily->parent()->associate($statistics);
+            }
+
+            $startingHours = $finishingHours = $preferredWorkouts = [];
+            foreach ($workoutsBasedOnDay as $workout){
+                isset($startingHours[$workout->startingHour]) ? $startingHours[$workout->startingHour]++ : $startingHours[$workout->startingHour] = 0;
+                isset($finishingHours[$workout->finishHour]) ? $finishingHours[$workout->finishHour]++ : $finishingHours[$workout->finishHour] = 0;
+                isset($preferredWorkouts[$workout->type]) ? $preferredWorkouts[$workout->type]++ : $preferredWorkouts[$workout->type] = 0;
+            }
+
+            $daily->preferredStartingHour = array_search(max($startingHours), $startingHours);
+            $daily->preferredFinishingHour = array_search(max($finishingHours), $finishingHours);
+            $daily->preferredWorkout = array_search(max($preferredWorkouts), $preferredWorkouts);
+
+            $daily->save();
+        }
     }
 }
